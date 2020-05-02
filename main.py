703474776cba8060784d1dc8d4c43d843f793a8d
KateMeon <katerina03535@yandex.ru>
2020-05-02 18:05:04 +0700

    HTML forms

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ app.config['SECRET_KEY'] = '_secret_key'
 @app.route('/')
 @app.route('/main_page')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', title='ABOUT MOVIES')
 
 
 @app.route('/registration')
 def registration_form():
-    return render_template('registration.html')
+    return render_template('registration.html', title='Регистрация')
+
+
+@app.route('/login')
+def login_form():
+    return render_template('login.html', title='Вход')
 
 
 if __name__ == '__main__':
